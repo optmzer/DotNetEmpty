23db9e26a6ce34ae304cf2063f82015a16b0d7b8
Alexander Frolov <big_air2409@mail.ru>
2019-01-06 17:27:29 +1300

    added Db migration and updated tables on Azure server.

---
+++ Scoreboards.Data/Migrations/20190106041443_Added user role.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scoreboards.Data.Migrations
+{
+    public partial class Addeduserrole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RoleCreatinonDate",
+                table: "AspNetRoles",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "RoleCreatinonDate",
+                table: "AspNetRoles");
+        }
+    }
+}
