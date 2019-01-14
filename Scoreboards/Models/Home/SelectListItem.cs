855e6f1a0261c500e3a48e18101a19e7b8413dc7
JlewisPk <lewis4692@hotmail.com>
2019-01-14 16:37:27 +1300

    home page now shows leaderboard&history and filtered by drop down list

---
+++ Scoreboards/Models/Home/SelectListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scoreboards.Models.Home
+{
+    public class SelectListItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
