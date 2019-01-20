0bec384d72ca33a60a08206d5686f48492cf17f6
Alexander Frolov <big_air2409@mail.ru>
2019-01-21 09:50:29 +1300

    Added UploadService to handle Blob storage uploads

---
+++ Scoreboard.Services/UploadService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Scoreboards.Data;
+
+/**
+ * Manages upload of User Profile Picture to Azure Blob Sotrage
+ * 
+ */ 
+
+namespace Scoreboards.Services
+{
+    public class UploadService : IUpload
+    {
+        public CloudBlobContainer GetStorageContainer(string blobStorageConnectionString)
+        {
+            var storageAccount = CloudStorageAccount.Parse(blobStorageConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            return blobClient.GetContainerReference("userimages");
+        }
+    }
+}
---
+++ Scoreboards.Data/IUpload.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Scoreboards.Data
+{
+    public interface IUpload
+    {
+        CloudBlobContainer GetStorageContainer(string blobStorageConnectionString);
+    }
+}
