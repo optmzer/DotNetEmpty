9c128cb8dee9fa0c20ef44595c73c0ae37be16ec
Alexander Frolov <big_air2409@mail.ru>
2019-01-21 15:34:44 +1300

    Added user avatar to _LoginPartial

---
+++ Scoreboards/Views/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Scoreboards.Data;
+using Scoreboards.Data.Models;
+using System.Threading.Tasks;
+
+namespace Scoreboards.Views.Shared
+{
+    public class _LoginModel : PageModel
+    {
+        private readonly IApplicationUser _applicationUserService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public _LoginModel(
+            IApplicationUser applicationUserService,
+            UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _applicationUserService = applicationUserService;
+        }
+
+        public string ProfileImageUrl { get; set; }
+
+        public async Task OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            ProfileImageUrl = user.ProfileImageUrl;
+        }
+    }
+}
