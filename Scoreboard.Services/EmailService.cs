ee3076526fdf515fbc43dcd0dc8004d895a8b891
Alexander Frolov <big_air2409@mail.ru>
2019-01-24 15:08:10 +1300

    Added email account confirmation via SendGrid

---
+++ Scoreboard.Services/EmailService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Scoreboards.Services
+{
+    public class EmailService : IEmailSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return SendEmailAsyncSandGridAsync(email, subject, htmlMessage);
+        }
+
+        private async Task SendEmailAsyncSandGridAsync(string email, string subject, string htmlMessage)
+        {
+            //region formatter
+            string text = string.Format("Please click on this link to {0}: {1}", subject, htmlMessage);
+            string html = htmlMessage;
+
+            //html += HttpUtility.HtmlEncode(@"Or click on the copy the following link on the browser:" + message.Body);
+            //endregion
+
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("donotreply@theinstilleryscoreboards.com");
+            msg.To.Add(new MailAddress(email));
+            msg.Subject = subject;
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+            SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+            NetworkCredential credentials = new NetworkCredential(
+                _config["SendGridCredentials:SEND_GRID_USERNAME"],
+                _config["SendGridCredentials:SEND_GRID_PASSWORD"]);
+            smtpClient.Credentials = credentials;
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(msg);
+
+            await Task.CompletedTask;
+        }
+    }
+}
--- Scoreboards/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Scoreboards/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -39,7 +39,7 @@ namespace Scoreboards.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
             [DataType(DataType.Text)]
             [Display(Name = "User Name")]
             public string UserName { get; set; }
@@ -93,8 +93,8 @@ namespace Scoreboards.Areas.Identity.Pages.Account
                         values: new { userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email at The Instillery Scoreboards",
+                        $"Please confirm your account at The Instillery Scoreboards website by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
