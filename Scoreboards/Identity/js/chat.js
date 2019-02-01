8c2675926e9f071b2363133dfaf39f06d7c6e527
Alexander Frolov <big_air2409@mail.ru>
2019-02-01 14:11:44 +1300

    Fixed user logo in navbar for Account/Manage

---
+++ Scoreboards/Identity/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/scoreboardsHub").build();
+
+//On getting "ReceiveMessage" tag updates <ul> with new data (name, message)
+connection.on("Notify", function (message) {
+    //reload the page
+    location.reload();
+    //document.getElementById("liveMessageBar").innerHTML = message;
+});
+
+//Starts connection
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+// Reconnect client after inaction
+async function start() {
+    try {
+        await connection.start();
+        console.log('connected');
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
