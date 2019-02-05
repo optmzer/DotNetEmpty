97ae14812c46841eb5499a1957c2fb07dd0bade7
Alexander Frolov <big_air2409@mail.ru>
2019-02-05 14:32:13 +1300

    Added server side validation for NewUserGame form Player1 != Player2

---
+++ Scoreboards.Data/Validators/NotEqualAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+/**
+ * Throws error if 2 text fields are equal
+ */ 
+
+namespace Scoreboards.Data.Validators
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class NotEqualAttribute : ValidationAttribute, IClientModelValidator
+    {
+        private const string DefaultErrorMessage = "The value of {0} cannot be the same as the value of the {1}.";
+
+        private readonly string _otherProperty;
+
+        public NotEqualAttribute(string otherProperty) : base(DefaultErrorMessage)
+        {
+            _otherProperty = otherProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                var currentVal = (string)value;
+
+                var other_property = validationContext.ObjectType.GetProperty(_otherProperty);
+
+                if (other_property == null)
+                    throw new ArgumentException("Property with this name not found");
+
+                var otherPropertyVal = (string)other_property.GetValue(validationContext.ObjectInstance, null);
+
+                // Srings are equal throw error
+                if(String.Equals(currentVal, otherPropertyVal))
+                {
+                    return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+                }
+            }
+
+            return ValidationResult.Success;
+            //return base.IsValid(value, validationContext); // Automatically added
+        }
+
+
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            var error = FormatErrorMessage(context.ModelMetadata.GetDisplayName());
+            context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-equalto", error);
+            context.Attributes.Add("data-val-equalto-other", "*.User_Id_02");
+        }
+    }
+}
--- Scoreboards/Models/UserGames/NewUserGameModel.cs
+++ Scoreboards/Models/UserGames/NewUserGameModel.cs
@@ -1,4 +1,5 @@
 ﻿using Scoreboards.Data.Models;
+using Scoreboards.Data.Validators;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -12,16 +13,25 @@ namespace Scoreboards.Models.UserGames
         public IEnumerable<ApplicationUser> UsersList { get; set; }
 
         //Players detail
+        [Required]
+        [NotEqual("User_02_Id", ErrorMessage = "Player 1 and 2 should be different")]
         public string User_01_Id { get; set; }
+
+        [Required]
         public string User_01_Team { get; set; }
+
+        [Required]
         public string User_02_Id { get; set; }
+
+        [Required]
         public string User_02_Team { get; set; }
 
         //Score 
         //public string GameScore { get; set; }
-        [Range(0, 200, ErrorMessage = "Please enter integers in a range of {1} to {2}" )]
+        [Required]
         public int GameScoreUser01 { get; set; }
-        [Range(0, 200, ErrorMessage = "Please enter integers in a range of {1} to {2}")]
+
+        [Required]
         public int GameScoreUser02 { get; set; }
 
         //Winner, “USER_01_Id”, “USER_02_Id”, “DRAW”
